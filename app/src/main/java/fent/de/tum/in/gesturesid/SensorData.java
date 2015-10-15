8a53cf6b5a248aea6a1fd23c0d6914bff90e6b98
fent <philipp@fent.de>
2015-10-15 14:00:43 +0200

    add the ability to display data in GraphViews

--- app/src/main/java/fent/de/tum/in/gesturesid/SensorData.java
+++ app/src/main/java/fent/de/tum/in/gesturesid/SensorData.java
@@ -1,5 +1,9 @@
 package fent.de.tum.in.gesturesid;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -68,7 +72,7 @@ public class SensorData {
         }
     }
 
-    float[][] data;
+    private final float[][] data;
 
     public SensorData(float[][] data) {
         this.data = data;
@@ -97,6 +101,7 @@ public class SensorData {
 
     /**
      * Smooth the dataSet to better localize the distinctive values
+     *
      * @param alpha the exponential factor: in rage (0,1). Lower alpha means slower averaging
      */
     private static void exponentiallySmoothData(float[][] dataSet, float alpha) {
@@ -111,6 +116,29 @@ public class SensorData {
                 oldValue = newValue;
                 dataRow[i] = newValue;
             }
+        }
+    }
+
+    public void displayData(GraphView[] views) {
+        if (views.length != data.length) {
+            throw new IllegalArgumentException();
+        }
+
+        final int vectorSize = data.length;
+
+        for (int i = 0; i < vectorSize; i++) {
+            final float[] dataRow = data[i];
+            final DataPoint[] dataPointRow = new DataPoint[dataRow.length];
+            final int dataRowSize = data[i].length;
+
+            for(int j = 0; j < dataRowSize; j++) {
+                dataPointRow[j] = new DataPoint(j, dataRow[j]);
+            }
+
+            // now set this dataPointRow to the appropriate View
+            final GraphView view = views[i];
+            view.removeAllSeries();
+            view.addSeries(new LineGraphSeries<>(dataPointRow));
         }
     }
 }
