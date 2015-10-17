91f4407d009d5a4c5d7440b1952e8e892e2720e1
fent <philipp@fent.de>
2015-10-17 15:39:26 +0200

    implement a ViewPager to display the Activity's content

--- app/src/main/java/fent/de/tum/in/gesturesid/MeasurementActivity.java
+++ app/src/main/java/fent/de/tum/in/gesturesid/MeasurementActivity.java
@@ -3,6 +3,10 @@ package fent.de.tum.in.gesturesid;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,12 +15,34 @@ import android.view.MenuItem;
 
 public class MeasurementActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measurement);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        viewPager =  (ViewPager) findViewById(R.id.vPager);
+        pagerAdapter = new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return new MeasurementActivityFragment();
+                    default:
+                        return null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 1;
+            }
+        };
+        viewPager.setAdapter(pagerAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
