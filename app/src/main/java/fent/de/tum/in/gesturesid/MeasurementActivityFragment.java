240a1422030620261fc8034b7d2f9367983d2624
fent <philipp@fent.de>
2015-10-23 13:42:16 +0200

    Improve password input behaviour

--- app/src/main/java/fent/de/tum/in/gesturesid/MeasurementActivityFragment.java
+++ app/src/main/java/fent/de/tum/in/gesturesid/MeasurementActivityFragment.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 /**
@@ -54,6 +55,7 @@ public class MeasurementActivityFragment extends Fragment {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (hasFocus) {
+                    ((EditText) v).setText(null);
                     builder = null;
                     manager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_FASTEST);
                     Log.d("Test", "EditText  got focus");
@@ -62,6 +64,14 @@ public class MeasurementActivityFragment extends Fragment {
                 manager.unregisterListener(listener);
                 callback.OnPatternReceived(builder.toSensorData());
                 Log.d("Test", "EditText lost focus");
+
+                // Hide the keyboard
+                InputMethodManager manager = (InputMethodManager) v.getContext()
+                        .getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (manager != null) {
+                    manager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+
             }
         });
         return view;
